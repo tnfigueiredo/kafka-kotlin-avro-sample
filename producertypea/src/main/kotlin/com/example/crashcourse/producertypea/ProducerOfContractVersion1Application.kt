fa603abe7390b1589991eb7b8f956f8eade89629
tnfigueiredo <tnfigueiredo@gmail.com>
2022-07-31 02:50:37 +0100

    Initial version of producer project from spring initializer for version 1 of avro contract message.

---
+++ producertypea/src/main/kotlin/com/example/crashcourse/producertypea/ProducerOfContractVersion1Application.kt
@@ -0,0 +1,11 @@
+package com.example.crashcourse.producertypea
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProducerOfContractVersion1Application
+
+fun main(args: Array<String>) {
+	runApplication<ProducerOfContractVersion1Application>(*args)
+}
---
+++ producertypea/src/test/kotlin/com/example/crashcourse/producertypea/ProducerOfContractVersion1ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.crashcourse.producertypea
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProducerOfContractVersion1ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
